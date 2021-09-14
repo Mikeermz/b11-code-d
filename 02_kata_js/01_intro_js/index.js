f47bfe9548d76baa87ca8b3326bb3aa6280ed637
Mikee Ramirez <mikee.rmz@gmail.com>
2021-09-13 22:29:38 -0500

    Feat: adding intro to js
    Signed-off-by: Mikee Ramirez <mikee.rmz@gmail.com>

---
+++ 02_kata_js/01_intro_js/index.js
@@ -0,0 +1,35 @@
+
+console.log('Hola a todos desde la consola', ' estoy en clase');
+console.log('suma de 1 + 2 =', 1 + 2);
+console.log('4 es menor a 3 ', 4 < 3);
+console.info('Yo soy un console de informacion');
+console.error('Yo soy un error');
+console.warn('yo soy un warning');
+
+document.write('<br> Hola a todos desde JS');
+/* 
+hola a todos 
+yo soy un comentario 
+de multiple linea
+*/
+
+// Salida alert
+// alert('hola desde otro archivo');
+
+// Entrada Prompt
+// console.log('Hola', prompt('Cual es tu nombre'));
+
+
+// Comentarios
+
+// hola a todos 
+// yo soy un comentario 
+// de una linea
+
+/* 
+  hola a todos 
+  yo soy un comentario 
+  de multiple linea
+*/
+
+
