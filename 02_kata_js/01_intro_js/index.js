f995e9bcbe75ab5e4e933551f642b417fae9f662
Mikee Ramirez <mikee.rmz@gmail.com>
2021-09-14 21:34:18 -0500

    chore: simple sweet sintax
    Signed-off-by: Mikee Ramirez <mikee.rmz@gmail.com>

--- 02_kata_js/01_intro_js/index.js
+++ 02_kata_js/01_intro_js/index.js
@@ -1,4 +1,4 @@
-
+// Mensaje que aparece en la consola del inspector de elementos.
 console.log('Hola a todos desde la consola', ' estoy en clase');
 console.log('suma de 1 + 2 =', 1 + 2);
 console.log('4 es menor a 3 ', 4 < 3);
@@ -6,19 +6,20 @@ console.info('Yo soy un console de informacion');
 console.error('Yo soy un error');
 console.warn('yo soy un warning');
 
-document.write('<br> Hola a todos desde JS');
+// Escribir directo en el DOM con JS
+document.write('<br> <div>Hola a todos desde JS</div>');
+
 /* 
 hola a todos 
 yo soy un comentario 
 de multiple linea
 */
 
-// Salida alert
-// alert('hola desde otro archivo');
-
 // Entrada Prompt
 // console.log('Hola', prompt('Cual es tu nombre'));
 
+// Salida alert
+// alert('hola desde otro archivo');
 
 // Comentarios
 
