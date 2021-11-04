54c7e4cf7c96f1fa625fee793cf1d86a2caf8e2a
Mikee Ramirez <mikee.rmz@gmail.com>
2021-11-03 21:42:10 -0600

    feat: adding excersises petition
    Signed-off-by: Mikee Ramirez <mikee.rmz@gmail.com>

---
+++ 04_kata_js_avanzado/02_peticiones/peticion_pokemon/main.js
@@ -0,0 +1,76 @@
+const request = require("request");
+// Hacer una petición a la PokeAPI para obtener cualquier Pokémon.  Muestra sus tipos en consola mediante un for. https://pokeapi.co/
+
+const getPokemonByName = (pokemon) => {
+  const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+
+  request.get(URL, (error, response, body) => {
+    if (error || response.statusCode === 404) {
+      console.log(error);
+      return null;
+    }
+    const pokemon = JSON.parse(body)
+
+    const {name, types} = pokemon;
+
+    const type = types.map((type) => {
+      const {type: { name }} = type
+      return name;
+    });
+
+    console.log({name, type})
+  }); 
+};
+
+// getPokemonByName('dragonite')
+
+
+// Escribe una función que al ejecutarse realice una petición a la API de Open Library.
+// (Ejemplo: peticionLibro("i robot");
+// Buscar un libro y traer el o los autores del primer libro
+// http://openlibrary.org/search.json?q=i+robot) 
+
+
+const getAuthorByBook = (book) => {
+  const URL = `http://openlibrary.org/search.json?q=${book}`;
+
+  request.get(URL, (error, response, body) => {
+    const { docs } = JSON.parse(body);
+
+    if (error || response.statusCode === 404 || docs.length === 0) {
+      console.log(error);
+      return null;
+    }
+
+    const [ firstBook ] = docs;
+    
+    const { author_name } = firstBook;
+
+    console.log({ authors: author_name})
+    
+  }); 
+};
+
+// getAuthorByBook('divina comedia');
+
+// Hacer una petición por autor y devolver la lista de sus libros
+// http://openlibrary.org/search.json?author=asimov
+
+const getBooksByAuthor= (author) => {
+  const URL = `http://openlibrary.org/search.json?author=${author}`;
+
+  request.get(URL, (error, response, body) => {
+    const { docs } = JSON.parse(body);
+
+    if (error || response.statusCode === 404 || docs.length === 0) {
+      console.log(error);
+      return null;
+    }
+    
+    const books =  docs.map((book) => book.title);
+
+    console.log({books})
+  }); 
+};
+
+// getBooksByAuthor('asimov');
