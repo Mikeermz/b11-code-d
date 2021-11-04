c907bdc27461172f40a0fe11cdd736dab2dfa1ca
Mikee Ramirez <mikee.rmz@gmail.com>
2021-11-03 19:39:34 -0600

    feat: adding google
    Signed-off-by: Mikee Ramirez <mikee.rmz@gmail.com>

---
+++ 04_kata_js_avanzado/02_peticiones/petciion_google/command.js
@@ -0,0 +1,15 @@
+// Inicializar proyecto de node 
+// npm init
+// Aqui vienen las preguntas para crear repositorio
+// npm init -y 
+// Aqui las preguntas las asume todo como que si. 
+
+// Instalar dependecia
+// npm install --save <dependencia>
+// npm install <dependencia>
+// npm i <dependencia>
+
+// Instalar dependencia de desarrollo
+// npm install --save-dev <dependencia de desarrollo>
+// npm install -D <dependencia de desarrllo>
+// npm i -D <dependencia de desarrollo>
